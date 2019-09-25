70ad33d5a838e021f4c294bb78b7a2f047428fb2
iposton <ianposton@sbcglobal.net>
2019-09-25 11:09:49 -0700

    build element

--- build-elements.js
+++ build-elements.js
@@ -2,14 +2,14 @@ const fs = require('fs-extra');
 const concat = require('concat');
 (async function build() {
   const files = [
-    './dist/elements/runtime.js',
-    './dist/elements/polyfills.js',
+    './dist/elements/runtime-es2015.js',
+    './dist/elements/polyfills-es2015.js',
     './dist/elements/scripts.js',
-    './dist/elements/main.js',
+    './dist/elements/main-es2015.js',
   ]
   await fs.ensureDir('elements')
   await concat(files, 'elements/elements.js');
   await fs.copyFile('./dist/elements/styles.css', 'elements/styles.css')
-  await fs.copy('./dist/elements/assets/', 'elements/assets/' )
+  //await fs.copy('./dist/elements/assets/', 'elements/assets/' )
 
 })()
