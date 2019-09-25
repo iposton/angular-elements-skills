996814075e217695df1e02574351cac6d762cefd
iposton <ianposton@sbcglobal.net>
2019-09-25 11:32:22 -0700

    add code to element

--- projects/wizard-el/src/app/app.module.ts
+++ projects/wizard-el/src/app/app.module.ts
@@ -1,16 +1,39 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Injector } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WizardComponent } from './component/wizard/wizard.component';
+
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WizardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
+  entryComponents: [AppComponent,
+    WizardComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(AppComponent, 
+                                 { injector: this.injector });
+    customElements.define('my-own-element', el);
+    const el2 = createCustomElement(WizardComponent, 
+                                 { injector: this.injector });
+    customElements.define('wizard', el2);
+  }
+ }
